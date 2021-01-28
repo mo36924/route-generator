a1ca8a09c437063a698e9e5843286f72fee2359a
mo36924 <mo36924@users.noreply.github.com>
2021-01-28 20:37:18 +0900

    ✨

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,7 @@ const defaultOptions: Required<Options> = {
 };
 
 const defaultTemplate = `
+import changestate from "@mo36924/changestate";
 import lazy from "@mo36924/preact-lazy";
 import { ComponentType, createContext } from "preact";
 import { useState, useEffect } from "preact/hooks";
@@ -48,8 +49,10 @@ export type RouteContextValue = { url: URL; route: Route; props: Props };
 export const RouteContext = createContext((null as any) as RouteContextValue);
 export const { Provider, Consumer } = RouteContext;
 
-export const match = (href: string): RouteContextValue => {
-  const url = new URL(href);
+export const match = (url: string | URL): RouteContextValue => {
+  if (typeof url === "string") {
+    url = new URL(url);
+  }
   const path = url.pathname.slice(1);
   const route = staticRoutes[path]!;
   const props: Props = {};
@@ -68,78 +71,8 @@ export const match = (href: string): RouteContextValue => {
   return { url, route, props };
 };
 
-const changestate = "changestate";
-
-declare global {
-  interface Window {
-    onchangestate?: null | ((this: Window, ev: Event) => any);
-  }
-}
-
-if (typeof window !== "undefined" && window.onchangestate === undefined) {
-  window.onchangestate = null;
-
-  const createEvent =
-    typeof Event === "function"
-      ? (type: string) => new Event(type)
-      : (type: string) => {
-          const event = document.createEvent("Event");
-          event.initEvent(type, false, false);
-          return event;
-        };
-
-  const events = ["pushState", "replaceState"] as const;
-  const origin = location.origin + "/";
-
-  events.forEach((method) => {
-    const original = history[method].bind(history);
-    const type = method.toLowerCase();
-
-    history[method] = (...args) => {
-      original(...args);
-      dispatchEvent(createEvent(type));
-    };
-  });
-
-  [...events, "popstate"].forEach((type) => {
-    addEventListener(type.toLowerCase(), () => {
-      const event = createEvent(changestate);
-      dispatchEvent(event);
-
-      if (typeof window.onchangestate === "function") {
-        window.onchangestate(event);
-      }
-    });
-  });
-
-  addEventListener("click", (e) => {
-    if (e.ctrlKey || e.metaKey || e.altKey || e.shiftKey || e.button) {
-      return;
-    }
-
-    let t: any = e.target;
-
-    do {
-      if (\`\${t.nodeName}\`.toLowerCase() === "a") {
-        if (t.hasAttribute("download") || t.getAttribute("rel") === "external") {
-          return;
-        }
-
-        const url = t.href;
-
-        if (url.startsWith(origin)) {
-          e.preventDefault();
-          history.pushState(null, "", url);
-        }
-
-        return;
-      }
-    } while ((t = t.parentNode));
-  });
-}
-
-export default (props: { href: string }) => {
-  const [state, setState] = useState(match(props.href));
+export default (props: { url: string | URL }) => {
+  const [state, setState] = useState(match(props.url));
 
   useEffect(() => {
     const handleChangestate = () => {
@@ -196,10 +129,12 @@ export default async (options?: Options) => {
 
     const componentName = nonExtPath.replace(/[\/\-]/g, "$");
 
-    let routePath = nonExtPath
-      .replace(/^index$/, "")
-      .replace(/\/index$/, "/")
-      .replace(/__|_/g, (m) => (m === "_" ? ":" : "_"));
+    let routePath =
+      "/" +
+      nonExtPath
+        .replace(/^index$/, "")
+        .replace(/\/index$/, "/")
+        .replace(/__|_/g, (m) => (m === "_" ? ":" : "_"));
 
     const rank = routePath
       .split("/")
